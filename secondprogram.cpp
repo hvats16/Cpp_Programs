24f045884cf5532073ea3575fa36ba545ec9b14e
Harshit <hvats16@gmail.com>
2019-09-30 17:34:23 +0530

    program to add 2 no.

---
+++ secondprogram.cpp
@@ -0,0 +1,13 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+int a,b,c; 
+cout<<"enter first no.";
+cin>>a;
+cout<<"enter second no.";
+cin>>b;
+c=a+b;
+cout<<"sum is="<<c;
+getch();
+}
