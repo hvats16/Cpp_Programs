62b744b89fb00fcc0e7db7383a116cf763b46ead
Harshit <hvats16@gmail.com>
2019-10-02 13:08:33 +0530

    Program to multiply two no.

---
+++ FourthProgram.cpp
@@ -0,0 +1,13 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+cout<<"enter the first no.";
+cin>>a;
+cout<<"enter the second no.";
+cin>>b;
+c=a*b;
+cout<<"the product is="<<c;
+getch();
+}
